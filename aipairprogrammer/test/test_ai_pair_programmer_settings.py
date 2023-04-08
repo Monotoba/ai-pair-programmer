6eb6ad5ac0730eb738ecdf78e61c8c2b64d650c4
Randall Morgan <rmorgan62@gmail.com>
2023-04-07 19:08:52 -0600

    Fixed links in README.md file. Started adding tests to test_ai_pair_programmer_settings.py file.

--- aipairprogrammer/test/test_ai_pair_programmer_settings.py
+++ aipairprogrammer/test/test_ai_pair_programmer_settings.py
@@ -1,22 +1,35 @@
-def test_get_state():
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import pytest
+
+from aipairprogrammer.ai_pair_programmer_settings import AIPairProgrammerSettings
+
+
+@pytest.fixture()
+def test_settings():
+    settings = AIPairProgrammerSettings()
+    return settings
+
+def test_get_state(settings):
+    state_ = settings.get_state()
+    assert state_ == settings
+
+
+def test_load_state(settings):
     assert False
 
 
-def test_load_state():
+def test_save_state(settings):
     assert False
 
 
-def test_save_state():
+def test_get_api_key(settings):
     assert False
 
 
-def test_get_api_key():
+def test_set_api_key(settings):
     assert False
 
 
-def test_set_api_key():
-    assert False
-
-
-def test_get_model_name():
+def test_get_model_name(settings):
     assert False
